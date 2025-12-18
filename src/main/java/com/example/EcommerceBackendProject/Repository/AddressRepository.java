e937e089ae2f30405c8d607f91bb1095120f2ce7
namnguyen <namhoanghainguyen@gmail.com>
2025-12-17 20:56:26 -0700

    remove unused import

--- src/main/java/com/example/EcommerceBackendProject/Repository/AddressRepository.java
+++ src/main/java/com/example/EcommerceBackendProject/Repository/AddressRepository.java
@@ -1,7 +1,6 @@
 package com.example.EcommerceBackendProject.Repository;
 
 import com.example.EcommerceBackendProject.Entity.Address;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
--- src/main/java/com/example/EcommerceBackendProject/Service/AddressService.java
+++ src/main/java/com/example/EcommerceBackendProject/Service/AddressService.java
@@ -1,4 +1,6 @@
 package com.example.EcommerceBackendProject.Service;
 
 public class AddressService {
+
+    
 }
