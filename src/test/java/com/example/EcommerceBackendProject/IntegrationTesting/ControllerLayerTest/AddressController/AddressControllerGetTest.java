2ba2f812a0883829877b7570833983ecf568971e
namnguyen <namhoanghainguyen@gmail.com>
2026-04-08 19:16:40 -0600

    continue with test controller

--- src/test/java/com/example/EcommerceBackendProject/IntegrationTesting/ControllerLayerTest/AddressController/AddressControllerGetTest.java
+++ src/test/java/com/example/EcommerceBackendProject/IntegrationTesting/ControllerLayerTest/AddressController/AddressControllerGetTest.java
@@ -1,8 +1,10 @@
 package com.example.EcommerceBackendProject.IntegrationTesting.ControllerLayerTest.AddressController;
 
 import com.example.EcommerceBackendProject.Controller.AddressController;
+import com.example.EcommerceBackendProject.DTO.AddressResponseDTO;
 import com.example.EcommerceBackendProject.Entity.Address;
 import com.example.EcommerceBackendProject.Entity.User;
+import com.example.EcommerceBackendProject.Mapper.AddressMapper;
 import com.example.EcommerceBackendProject.Security.JwtService;
 import com.example.EcommerceBackendProject.Security.SecurityUtils;
 import com.example.EcommerceBackendProject.Service.AddressService;
@@ -50,28 +52,92 @@ public class AddressControllerGetTest {
         Address address = new Address(user, "123 Main st", "city", "state", "country", "12345", true);
         address.setId(1L);
         Address address1 = new Address(user, "1234 Main st", "city", "state", "country", "12345", false);
-        address.setId(2L);
+        address1.setId(2L);
         Address address2 = new Address(user, "1235 Main st", "city", "state", "country", "12345", false);
-        address.setId(3L);
+        address2.setId(3L);
         List<Address> addresses = List.of(address, address1, address2);
-        Pageable pageable = PageRequest.of(0, 10);
-        Page<Address> addressPage = new PageImpl<>(addresses, pageable, addresses.size());
+        Page<Address> pageAddress = new PageImpl<>(addresses);
 
         when(securityUtils.getCurrentUserId()).thenReturn(1L);
-        when(addressService.getUserAddresses(1L, pageable)).thenReturn(addressPage);
+        when(addressService.getUserAddresses(eq(1L), any(Pageable.class))).thenReturn(pageAddress);
 
-        mockMvc.perform(get(ADDRESS_URL))
+        mockMvc.perform(get(ADDRESS_URL)
+                        .param("page", "0")
+                        .param("size", "10"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.userId").value(1L))
-                .andExpect(jsonPath("$.street").value("123 Main st"))
-                .andExpect(jsonPath("$.city").value("city"))
-                .andExpect(jsonPath("$.state").value("state"))
-                .andExpect(jsonPath("$.country").value("country"))
-                .andExpect(jsonPath("$.zipCode").value("12345"))
-                .andExpect(jsonPath("$.isDefault").value(true));
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content.length()").value(3))
+                .andExpect(jsonPath("$.content[0].userId").value(1L))
+                .andExpect(jsonPath("$.content[0].street").value("123 Main st"))
+                .andExpect(jsonPath("$.content[0].city").value("city"))
+                .andExpect(jsonPath("$.content[0].state").value("state"))
+                .andExpect(jsonPath("$.content[0].country").value("country"))
+                .andExpect(jsonPath("$.content[0].zipCode").value("12345"))
+                .andExpect(jsonPath("$.content[0].isDefault").value(true));
 
         verify(securityUtils).getCurrentUserId();
-        verify(addressService).getDefaultAddress(1L);
+        verify(addressService).getUserAddresses(eq(1L), any(Pageable.class));
+        verifyNoMoreInteractions(addressService);
+    }
+
+    @Test
+    void getUserAddress_success_pagination() throws Exception {
+        User user = new User(null, "+1234567891", "last", "user",
+                "encodedPassword", "user1@gmail.com", "testuser");
+        user.setId(1L);
+        Address address = new Address(user, "123 Main st", "city", "state", "country", "12345", true);
+        address.setId(1L);
+        Address address1 = new Address(user, "1234 Main st", "city", "state", "country", "12345", false);
+        address1.setId(2L);
+        Address address2 = new Address(user, "1235 Main st", "city", "state", "country", "12345", false);
+        address2.setId(3L);
+        List<Address> addresses = List.of(address, address1, address2);
+
+        when(securityUtils.getCurrentUserId()).thenReturn(1L);
+        when(addressService.getUserAddresses(eq(1L), any(Pageable.class)))
+                .thenAnswer(inv -> {
+                    Pageable pageable = inv.getArgument(1);
+                    int page = pageable.getPageNumber();
+                    int size = pageable.getPageSize();
+
+                    int start = (int) pageable.getOffset();
+                    int end = Math.min(start + size, addresses.size());
+
+                    List<Address> subList = addresses.subList(start, end);
+
+                    return new PageImpl<>(subList, pageable, addresses.size());
+                });
+
+        mockMvc.perform(get(ADDRESS_URL)
+                        .param("page", "0")
+                        .param("size", "2"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content.length()").value(2))
+                .andExpect(jsonPath("$.content[0].userId").value(1L))
+                .andExpect(jsonPath("$.content[0].street").value("123 Main st"))
+                .andExpect(jsonPath("$.content[0].city").value("city"))
+                .andExpect(jsonPath("$.content[0].state").value("state"))
+                .andExpect(jsonPath("$.content[0].country").value("country"))
+                .andExpect(jsonPath("$.content[0].zipCode").value("12345"))
+                .andExpect(jsonPath("$.content[0].isDefault").value(true));
+
+        mockMvc.perform(get(ADDRESS_URL)
+                        .param("page", "1")
+                        .param("size", "2"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content.length()").value(1))
+                .andExpect(jsonPath("$.content[0].userId").value(1L))
+                .andExpect(jsonPath("$.content[0].street").value("1235 Main st"))
+                .andExpect(jsonPath("$.content[0].city").value("city"))
+                .andExpect(jsonPath("$.content[0].state").value("state"))
+                .andExpect(jsonPath("$.content[0].country").value("country"))
+                .andExpect(jsonPath("$.content[0].zipCode").value("12345"))
+                .andExpect(jsonPath("$.content[0].isDefault").value(false));
+
+        verify(securityUtils).getCurrentUserId();
+        verify(addressService).getUserAddresses(eq(1L), any(Pageable.class));
         verifyNoMoreInteractions(addressService);
     }
 }
